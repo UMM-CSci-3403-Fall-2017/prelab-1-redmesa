102b2b30b2cc7a27990cbc8b102239168a505fd1
Matt M <mitch723@morris.umn.edu>
2017-09-06 13:32:18 -0500

    added the regex.txt file, and ran wrap_contents.sh to produce the my_chart.html. Created the regex code using RegExr.

--- wrap_contents.sh
+++ wrap_contents.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+#Wrap_Contetns takes in a Filename, Header/Footer and pushed out an file. This "wraps" the header and footer around the mainbody of text, in this case the FileName.
+
+FileName=$1
+HeadorFoot=$2
+FileOutPut=$3
 
 
+cat ${HeadorFoot}_header.html $FileName ${HeadorFoot}_footer.html >> $FileOutPut
